25fa59eae5e665d81e634be8e88cde3c7f7aa233
ICX\aliaksei.yarmalchuk <aliakseiyarmalchuk@gmail.com>
2018-02-02 17:50:43 +0300

    111

---
+++ TestProjectCSharp/Pages/BasePage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProjectCSharp.Pages
+{
+    class BasePage:PageObject
+    {
+
+        private readonly IWebDriver driver;
+
+        public BasePage(IWebDriver driver)
+        {
+            OpenQA.Selenium.Support.PageObjects.
+            this.driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            PageFactory.InitElements(driver, this);
+            
+
+        }
+    }
+}
